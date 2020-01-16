2fd54aabf837d5866c1e030da530a8437136082e
Dennis Padiernos <depadiernos@gmail.com>
2020-01-15 19:33:06 -0600

    initialize and implement login/signup react frontend

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+import { Route } from "react-router-dom"
+import AuthForm from "./components/AuthForm"
+
+
+export default function App() {
+
+  return (
+    <div>
+      <Route
+          exact
+          path="/login"
+          render={props => <AuthForm {...props} isLogin/>}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => <AuthForm {...props} />}
+        />
+    </div>
+  )
+}
